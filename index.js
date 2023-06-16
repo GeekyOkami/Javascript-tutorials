a18196e55a6cc4aaebd2c1c08fc2c381f3e85271
GeekyOkami <97523703+GeekyOkami@users.noreply.github.com>
2023-06-16 16:38:47 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,58 @@
-let count=0;
+const timeDisplay=document.querySelector("#timeDisplay");
+const startBtn=document.querySelector("#startBtn");
+const pauseBtn=document.querySelector("#pauseBtn");
+const resetBtn=document.querySelector("#resetBtn");
 
-document.getElementById("decrease").onclick=function(){
-    count-=1;
-    Number(count);
-    document.getElementById("mainscreen").innerHTML=count;
+let startTime=0;
+let elapsedTime=0;
+let currentTime=0;
+let paused=true;
+let intervalId;
+let hrs=0;
+let mins=0;
+let secs=0;
+
+startBtn.addEventListener("click",()=>
+{
+    if (paused) {
+        paused=false;
+        startTime=Date.now()-elapsedTime;
+        intervalId=setInterval(updateTime,1000);
+    }
+});
+pauseBtn.addEventListener("click",()=>{
+    if(!paused){
+        paused=true;
+        elapsedTime=Date.now()-startTime;
+        clearInterval(intervalId);
+    }
+});
+resetBtn.addEventListener("click",()=>{
+    paused=true;
+    clearInterval(intervalId);
+    startTime=0;
+    elapsedTime=0;
+    currentTime=0;
+    hrs=0;
+    mins=0;
+    secs=0;
+    timeDisplay.textContent="00:00:00";
+});
+
+function updateTime(){
+    elapsedTime=Date.now()-startTime;
+
+    secs=Math.floor((elapsedTime/1000)%60);
+    mins=Math.floor((elapsedTime/(1000*60))%60);
+    hrs=Math.floor((elapsedTime/(1000*60*60))%60);
+
+    secs=pad(secs);
+    mins=pad(mins);
+    hrs=pad(hrs);
+
+    timeDisplay.textContent=`${hrs}:${mins}:${secs}`;
 }
 
-document.getElementById("reset").onclick=function(){
-    count=0;
-    Number(count);
-    document.getElementById("mainscreen").innerHTML=count;
-}
-
-document.getElementById("increase").onclick=function(){
-    count+=1;
-    Number(count);
-    document.getElementById("mainscreen").innerHTML=count;
+function pad(element){
+    return ("0"+element).length > 2 ? element: "0"+element; 
 }
