db28691ac84f507da718ccf78ada42c0c8e55fc3
GeekyOkami <97523703+GeekyOkami@users.noreply.github.com>
2023-07-08 16:46:30 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,90 @@
-let count=0;
+const cells=document.querySelectorAll(".cell");
+const statusText=document.querySelector("#statusContainer");
+const restartBtn=document.querySelector("#restartBtn");
 
-document.getElementById("decrease").onclick=function(){
-    count-=1;
-    Number(count);
-    document.getElementById("mainscreen").innerHTML=count;
+const winCondition=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+
+let options=["","","","","","","","",""];
+let currentPlayer="X";
+let running=true;
+
+initializeGame();
+
+function initializeGame(){
+    cells.forEach(cell=>cell.addEventListener("click",cellClicked));
+    restartBtn.addEventListener("click",restartGame);
+    statusText.textContent=`${currentPlayer}'s Turn`;
 }
 
-document.getElementById("reset").onclick=function(){
-    count=0;
-    Number(count);
-    document.getElementById("mainscreen").innerHTML=count;
+function cellClicked(){
+    const cellIndex=this.getAttribute("cellIndex");
+
+    if(options[cellIndex]!="" || !running){
+        return;
+    }
+
+    updateCell(this,cellIndex);
+    checkWinner();
 }
 
-document.getElementById("increase").onclick=function(){
-    count+=1;
-    Number(count);
-    document.getElementById("mainscreen").innerHTML=count;
+function updateCell(cell,index){
+    options[index]=currentPlayer;
+    cell.textContent=currentPlayer;
 }
+
+function changePlayer(){
+    currentPlayer=(currentPlayer=="X")? "O":"X";
+    statusText.textContent=`${currentPlayer}'s Turn`;
+}
+
+function checkWinner(){
+    let roundWon=false;
+
+    for(let i=0;i<winCondition.length;i++){
+        const condition=winCondition[i];
+        const cellA=options[condition[0]];
+        const cellB=options[condition[1]];
+        const cellC=options[condition[2]];
+
+        if(cellA=="" || cellB=="" || cellC==""){
+            continue;
+        }
+        if(cellA==cellB && cellB==cellC){
+            roundWon=true;
+            break;
+        }
+
+    }
+
+    if(roundWon){
+        statusText.textContent=`${currentPlayer} Wins!`;
+        running=false;
+    }
+
+    else if(!options.includes("")){
+        statusText.textContent=`Draw!`;
+        running=false;
+    }
+    else{
+        changePlayer();
+    }
+
+}
+
+function restartGame(){
+    options=["","","","","","","","",""];
+    currentPlayer="X";
+    statusText.textContent=`${currentPlayer}'s Turn`;
+    cells.forEach(cell=>cell.textContent="");
+    running=true;
+}
+
