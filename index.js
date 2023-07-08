7a5c2df6304e9cff9f18c8e087e4935bff824bfb
GeekyOkami <97523703+GeekyOkami@users.noreply.github.com>
2023-07-08 16:45:40 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,46 @@
-let count=0;
+const choicebtns=document.querySelectorAll(".choicebtn");
+const playertxt=document.querySelector("#playerText");
+const comptxt=document.querySelector("#computerText");
+const resulttxt=document.querySelector("#resultText");
 
-document.getElementById("decrease").onclick=function(){
-    count-=1;
-    Number(count);
-    document.getElementById("mainscreen").innerHTML=count;
+let player;
+let comp;
+let result;
+
+choicebtns.forEach(button=>button.addEventListener("click", ()=>{
+    player=button.textContent;
+    computerturn();
+    playertxt.textContent=`Player : ${player}`;
+    comptxt.textContent=`Computer : ${comp}`;
+    resulttxt.textContent=checkWinner();
+}));
+
+function computerturn(){
+    const num=Math.floor(Math.random()*3)+1;
+    switch(num){
+        case 1:
+            comp="Rock";
+            break;
+        case 2:
+            comp="Paper";
+            break;
+        case 3:
+            comp="Scissors";
+            break;
+    }
 }
 
-document.getElementById("reset").onclick=function(){
-    count=0;
-    Number(count);
-    document.getElementById("mainscreen").innerHTML=count;
-}
-
-document.getElementById("increase").onclick=function(){
-    count+=1;
-    Number(count);
-    document.getElementById("mainscreen").innerHTML=count;
+function checkWinner(){
+    if(player==comp){
+        return "Draw!";
+    }
+    else if(player=="Rock"){
+        return comp=="Scissors" ? "Result : Player Wins!":"Result : Computer Wins!";
+    }
+    else if(player=="Paper"){
+        return comp=="Rock" ? "Result : Player Wins!":"Result : Computer Wins!";
+    }
+    else if(player=="Scissors"){
+        return comp=="Paper" ? "Result : Player Wins!":"Result : Computer Wins!";
+    }
 }
